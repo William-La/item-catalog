f3ed0692c37d2ecdf247f44368f839eb93195fd8
William-La <wla@g.hmc.edu>
2019-07-26 11:22:16 -0700

    edit file according to PEP8 guidelines

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -3,45 +3,50 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 from passlib.apps import custom_app_context as pwd_context
-import random, string
-from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+import random
+import string
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
+                          BadSignature, SignatureExpired)
 
 Base = declarative_base()
-sKey = "".join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+sKey = "".join(random.choice(string.ascii_uppercase +
+                             string.digits) for x in xrange(32))
+
 
 class Category(Base):
     __tablename__ = 'category'
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key=True)
     name = Column(String)
 
     @property
     def serialize(self):
         return{
-            'id' = self.id,
-            'name' = self.name
+            'id': self.id,
+            'name': self.name
         }
 
 
 class Item(Base):
     __tablename__ = 'item'
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key=True)
     cat_id = Column(Integer, ForeignKey('category.id'))
     title = Column(String)
     desc = Column(String)
-    
+
     @property
     def serialize(self):
         return{
-            'id' = self.id,
-            'cat_id' = self.cat_id,
-            'title' = self.title,
-            'desc' = self.desc
+            'id': self.id,
+            'cat_id': self.cat_id,
+            'title': self.title,
+            'desc': self.desc
         }
+
 
 class User(Base):
     __tablename__ = 'user'
-    id = Column(Integer, primary_key = True)
-    user = Column(String(32), index = True)
+    id = Column(Integer, primary_key=True)
+    user = Column(String(32), index=True)
     pic = Column(String)
     email = Column(String)
     pHash = Column(String(64))
@@ -53,24 +58,24 @@ class User(Base):
         return pwd_context.verify(password, self.password_hash)
 
     def gen_auth_token(self, expiration=3600):
-    	s = Serializer(sKey, expires_in = expiration)
-    	return s.dumps({'id':self.id})
+        s = Serializer(sKey, expires_in=expiration)
+        return s.dumps({'id': self.id})
 
     @staticmethod
     def verify_auth_token(token):
-    	s = Serializer(sKey)
-    	try:
-    		d = s.loads(token)
+        s = Serializer(sKey)
+        try:
+            d = s.loads(token)
         # expired token
-    	except SignatureExpired:
-    		return None
+        except SignatureExpired:
+            return None
         # invalid token
-    	except BadSignature:
-    		return None
-    	user_id = d['id']
-    	return user_id
+        except BadSignature:
+            return None
+        user_id = d['id']
+        return user_id
 
 
 engine = create_engine('sqlite:///itemCatalog.db')
- 
+
 Base.metadata.create_all(engine)
