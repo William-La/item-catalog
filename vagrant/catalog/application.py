b3c8d0eb090dfdfffd2a174fc04f083b4255f60d
William-La <wla@g.hmc.edu>
2019-07-29 12:56:31 -0700

    add route declarations and functions for basic web functionality

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -17,6 +17,60 @@ session = DBSession()
 app = Flask(__name__)
 
 
+@app.route("/")
+def landingPage():
+    recentItems = session.query(Item).order_by(Item.id)[0:9]
+    return render_template('landing.html', recentItems = recentItems)
+
+
+@app.route("/catalog/<String: category>/items")
+def showCategory(category):
+    cat_id = session.query(Category).filter_by(name = category).first().id
+    items = sesison.query(Item).filter_by(cat_id = cat_id).all
+    return render_template("category.html", items = items)
+
+
+@app.route("/catalog/(String: category>/<String: itemTitle>")
+def showItem(category, itemTitle):
+    cat_id = session.query(Category).filter_by(name = category).first().id
+    item = session.query(Item).filter_by(cat_id = cat_id, title = itemTitle).first()
+    return render_template("item.html", item = item)
+
+
+@app.route("/catalog/<String: itemTitle>/edit", methods=['GET','POST'])
+def editItem(itemTitle):
+    item = session.query(Item).filter_by(title = itemTitle).first()
+    if request.method == 'POST':
+        if request.form['title']:
+            item.title = request.form['title']
+        if request.form['desc']:
+            item.desc = request.form['desc']
+        if request.form['category']:
+            new_cat_id = session.query(Category).filter_by(
+                         name = request.form['category']).first().id
+            item.cat_id = new_cat_id
+            item.category = request.form['category']
+        session.add(item)
+        session.commit()
+        flash('Item Successfully Edited')
+        return redirect(url_for('showCategory', category = item.category))
+    else:
+        return render_template("edit.html", item = item)
+
+
+@app.route("/catalog/<String: itemTitle>/delete", methods=['GET','POST'])
+def deleteItem(itemTitle):
+    item = session.query(Item).filter_by(title = itemTitle).first()
+    category = item.category
+    if request.method == 'POST':
+        session.delete(item)
+        session.commit()
+        flash('Item Successfully Deleted')
+        return redirect(url_for('showCategory', category = category))
+    else:
+        return render_template("delete.html", item = item)
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=8000)
